56a300c6e494c1a2dd6eba64c94d30243c277739
Alexander Giang <alexgiang96@gmail.com>
2019-07-23 10:45:34 -0700

    Fixed problem where it would add existing exercises to the search array

--- src/App.js
+++ src/App.js
@@ -2,37 +2,68 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Createable from 'react-select/createable';
+//import Createable from 'react-select/createable';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', output: ''};
+    this.state = {
+      value: '', 
+      output: '',
+      exerciseList: [
+        "push ups",
+        "sit ups",
+        "bench press"
+      ]
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({
+      value: event.target.value,
+    });
   }
   
+  // if exercise isn't in the list, then add it to the list 
+  // otherwise, output the chosen exercise
   handleSubmit(event) {
-    this.setState({output: this.state.value});
+    let currentValue = this.state.value;
+    if(currentValue in this.state.exerciseList){
+      this.setState({
+        output: this.state.value,
+        exerciseList: this.state.exerciseList.concat(this.state.value)
+      });
+    }
+    else{
+      this.setState({
+        output: this.state.value,
+      });
+    }
+      
     event.preventDefault(); //prevents refresh after submit button
   }
+
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-        < label>
-            Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
+        {/* <form> */}
+          <label>
+            Exercises: 
+            <input type="text" value={this.state.value} list="exerciseList" onChange={this.handleChange} />
+            <datalist id="exerciseList">
+              {this.state.exerciseList.map((ex,key) =>
+                <option key={key} value={ex}/>
+                )}
+            </datalist>
           </label>
           <input type="submit" value="Submit" />
         </form>
-        <h1>{this.state.output}</h1>
+        <p>Chosen Exercise: {this.state.output}</p>
       </div>
       
     );
